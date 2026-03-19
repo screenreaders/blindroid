200a61ead902731a0abc702474fa88ce84360ede
screenreaders <screenreaders@gmail.com>
2026-03-19 23:13:58 +0100

    Bump version to 023

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.screenreaders.blindroid"
         minSdk = 29
         targetSdk = 35
-        versionCode = 22
-        versionName = "022"
+        versionCode = 23
+        versionName = "023"
     }
 
     val keystorePropsFile = rootProject.file("keystore.properties")
