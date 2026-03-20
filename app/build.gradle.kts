1d9c128b26a7be01ed59bb824b34c88a181de145
screenreaders <screenreaders@gmail.com>
2026-03-20 15:02:11 +0100

    Rename navigation to Talking Map

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.screenreaders.blindroid"
         minSdk = 29
         targetSdk = 35
-        versionCode = 48
-        versionName = "048"
+        versionCode = 49
+        versionName = "049"
     }
 
     val keystorePropsFile = rootProject.file("keystore.properties")
