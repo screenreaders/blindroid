e9cbc0d3c49fcbbf09fc5186e842948a2866450a
screenreaders <screenreaders@gmail.com>
2026-03-21 21:48:00 +0100

    v092 feed color diff

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.screenreaders.blindroid"
         minSdk = 29
         targetSdk = 36
-        versionCode = 91
-        versionName = "091"
+        versionCode = 92
+        versionName = "092"
     }
 
     val keystorePropsFile = rootProject.file("keystore.properties")
