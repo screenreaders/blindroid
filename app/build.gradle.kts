af90e63bfb87657d755575287d40f3846df10e98
screenreaders <screenreaders@gmail.com>
2026-03-25 11:01:30 +0100

    chore: v175 quick menu defaults

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.screenreaders.blindroid"
         minSdk = 29
         targetSdk = 36
-        versionCode = 174
-        versionName = "174"
+        versionCode = 175
+        versionName = "175"
     }
 
     val keystorePropsFile = rootProject.file("keystore.properties")
